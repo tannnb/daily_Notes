970a2357922f74fde0dec382ad826246f8398a9b
tannnb <tanbingwork@163.com>
2018-04-22 14:51:31 +0800

    feat:员工信息筛选练习

---
+++ Basics/Management/js/data.js
@@ -0,0 +1,24 @@
+let data = [
+	{
+		id: Symbol(),
+		name: "张三",
+		age: 18,
+		gender: "男",
+		state: true,
+		checked: false
+	},{
+		id: Symbol(),
+		name: "李四",
+		age: 19,
+		gender: "男",
+		state: false,
+		checked: false
+	},{
+		id: Symbol(),
+		name: "红五",
+		age: 19,
+		gender: "女",
+		state: false,
+		checked: false
+	}
+];
